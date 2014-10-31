77c2a679269fd3c2e12fb0714fb9b603e14f315b
Lindsay Thurmond <lindsay.thurmond@makeandbuild.com>
2014-10-31 10:17:40 -0400

    headers again

--- server/routes.js
+++ server/routes.js
@@ -19,17 +19,18 @@ module.exports = function (app) {
     app.use(function (req, res, next) {
 
         // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://arrontedesign.com');
+//        res.setHeader('Access-Control-Allow-Origin', 'http://arrontedesign.com');
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
         // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token,username,password,Accept');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,x-access-token,username,password,Accept');
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
+//        res.setHeader('Access-Control-Allow-Credentials', true);
 
         // Pass to next layer of middleware
         next();
