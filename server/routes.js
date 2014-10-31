d0c1df9a8ffb2907631eb95eb6f7e9efc81a03f1
Lindsay Thurmond <lindsay.thurmond@makeandbuild.com>
2014-10-31 10:10:05 -0400

    trying different headers

--- server/routes.js
+++ server/routes.js
@@ -15,11 +15,31 @@ module.exports = function (app) {
 //        next();
 //    });
 
-    app.all('*', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // Add headers
+    app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://arrontedesign.com');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token,username,password,Accept');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
         next();
     });
+
+//    app.all('*', function(req, res, next) {
+//        res.header("Access-Control-Allow-Origin", "*");
+//        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//        next();
+//    });
 
     // Insert routes below
     app.use('/api/tokens', require('./api/token'));
