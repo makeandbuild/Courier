7885dc0e0e2d58a64a6f2b83f2927e970d63ffaa
Lindsay Thurmond <lindsay.thurmond@makeandbuild.com>
2014-11-03 20:10:59 -0500

    add option range field for beacons

--- server/models/beacon.model.js
+++ server/models/beacon.model.js
@@ -4,12 +4,17 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var BeaconSchema = new Schema({
-  name: String,
-  uuid: String,
-  major: Number,
-  minor: Number,
-  properties: Schema.Types.Mixed,
-  active: Boolean
+    name: String,
+    uuid: String,
+    major: Number,
+    minor: Number,
+    properties: Schema.Types.Mixed,
+    active: Boolean,
+    /**
+     * Max distance a beacon can hit before being considered out of range.
+     * If not set all detections will be considered in range.
+     */
+    range: Number
 });
 
 module.exports = mongoose.model('Beacon', BeaconSchema);
